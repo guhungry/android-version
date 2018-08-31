4d59ef3f2cf4474b1e97a71c8fd1463c25da1bd0
Woraphot Chokratanasombat <wc@asiola.com>
2018-09-01 02:33:30 +0700

    [Version Number] Change: Add Comparable

--- version/src/main/java/com/guhungry/android/version/VersionNumber.kt
+++ version/src/main/java/com/guhungry/android/version/VersionNumber.kt
@@ -1,15 +1,31 @@
 package com.guhungry.android.version
 
-class VersionNumber(val version: String) {
+data class VersionNumber(val version: String): Comparable<VersionNumber> {
     val MAJOR: Int
     val MINOR: Int
     val PATCH: Int
 
+    //////////////
+    // Constructor
+    //////////////
     init {
         val matches = versionMatcher(version)
         MAJOR = versionAt(matches, 1)
         MINOR = versionAt(matches, 3)
         PATCH = versionAt(matches, 5)
+    }
+
+    /////////////
+    // Comparable
+    /////////////
+    override fun compareTo(other: VersionNumber): Int {
+        if (MAJOR > other.MAJOR) return 1
+        if (MAJOR < other.MAJOR) return -1
+        if (MINOR > other.MINOR) return 1
+        if (MINOR < other.MINOR) return -1
+        if (PATCH > other.PATCH) return 1
+        if (PATCH < other.PATCH) return -1
+        return 0
     }
 
     //////////////////
--- version/src/test/java/com/guhungry/android/version/VersionNumberTest.kt
+++ version/src/test/java/com/guhungry/android/version/VersionNumberTest.kt
@@ -21,4 +21,43 @@ object VersionNumberTest : Spek({
             }
         }
     }
+
+    given("version 1.3.5 and 1.3.5") {
+        val sut1 = VersionNumber("1.3.5")
+        val sut2 = VersionNumber("1.3.5")
+
+        on("compare") {
+            it("should equal to") {
+                assertThat(sut1 == sut2, equalTo(true))
+                assertThat(sut1 > sut2, equalTo(false))
+                assertThat(sut1 < sut2, equalTo(false))
+            }
+        }
+    }
+
+    given("version 2.4.1 and 1.2.2") {
+        val sut1 = VersionNumber("2.4.1")
+        val sut2 = VersionNumber("1.2.2")
+
+        on("compare") {
+            it("should more than") {
+                assertThat(sut1 == sut2, equalTo(false))
+                assertThat(sut1 > sut2, equalTo(true))
+                assertThat(sut1 < sut2, equalTo(false))
+            }
+        }
+    }
+
+    given("version 2.1.4 and 5.7.1") {
+        val sut1 = VersionNumber("2.1.4")
+        val sut2 = VersionNumber("5.7.1")
+
+        on("compare") {
+            it("should less than") {
+                assertThat(sut1 == sut2, equalTo(false))
+                assertThat(sut1 > sut2, equalTo(false))
+                assertThat(sut1 < sut2, equalTo(true))
+            }
+        }
+    }
 })
